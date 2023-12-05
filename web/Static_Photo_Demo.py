29cca2f888422c685e50b6e908fd4444d62505b5
Juliet Lawton <124414966+julietlawton@users.noreply.github.com>
2023-12-04 16:58:42 -0800

    Minor formatting change

--- web/Static_Photo_Demo.py
+++ web/Static_Photo_Demo.py
@@ -34,7 +34,7 @@ if image_file is not None:
         emotion, confidence = detect_emotion(img, bbox)
         img = draw_bounding_box(img, bbox, emotion_label=emotion, confidence_level=confidence)
 
-        st.image(img, width=700, channels="BGR")
+        st.image(img, width=700, channels="BGR", use_column_width="always")
         st.success("Success")
     else:
         st.error("Image is too small. Please upload an image bigger than 192x192 pixels.")
@@ -42,4 +42,4 @@ else:
     img = cv2.imread("web/smiling_man_placeholder.jpg")
     bbox = get_bounding_box(img)
     img = draw_bounding_box(img, bbox, emotion_label="Happy", confidence_level=0.88)
-    st.image(img, width=700, channels="BGR")
+    st.image(img, width=700, channels="BGR", use_column_width="always")
